99310cecd5dbdda4dd619743ca07ab2a0c0709a8
Filiz Bagdatli <melihahalim@hotmail.de>
2024-03-26 13:16:11 +0100

    I have added all update  to proje3

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,6 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("I am going to  lear Github");
+        System.out.println("I am going to  learn Github");
+        System.out.println(" I am learning Github");
     }
 }
